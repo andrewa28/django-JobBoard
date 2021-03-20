6b8326942fb9424057e9ce0dfa84089937b6deee
Andrew Albert <andrew@MacBook-Pro.local>
2021-03-20 02:40:13 +0200

    add job views & url

--- job/models.py
+++ job/models.py
@@ -22,6 +22,13 @@ class Job(models.Model): #table
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE) #one to many (one job in one category)
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
